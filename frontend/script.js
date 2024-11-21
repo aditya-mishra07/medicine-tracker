5d75ea2b5fe6513d1dcb44b878f51d443c78e13b
mishra-adi-07 <mishra.adi2005@gmail.com>
2024-11-21 01:59:58 +0000

    Fetched data from backend and displayed in table form

---
+++ frontend/script.js
@@ -0,0 +1,19 @@
+function fetchAllMedicine() {
+  const tbody = document.getElementById("tbody");
+  fetch("http://localhost:8000/medicines")
+    .then((response) => response.json())
+    .then((data) => {
+      if (data && data.medicines) {
+        data.medicines.forEach((medicine) => {
+          const row = document.createElement("tr");
+          row.innerHTML = `
+          <td>${medicine.name}</td>
+          <td>${medicine.price}</td>
+        `;
+          tbody.appendChild(row);
+        });
+      }
+    });
+}
+
+fetchAllMedicine();
